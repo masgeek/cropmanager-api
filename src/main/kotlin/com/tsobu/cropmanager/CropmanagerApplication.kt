d9ba0027463646ffbd99adce6760ad736c847a23
Sammy Barasa <barsamms@gmail.com>
2019-01-23 06:47:52 +0300

    Initial commit

---
+++ src/main/kotlin/com/tsobu/cropmanager/CropmanagerApplication.kt
@@ -0,0 +1,12 @@
+package com.tsobu.cropmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CropmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CropmanagerApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/tsobu/cropmanager/CropmanagerApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.tsobu.cropmanager
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CropmanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
